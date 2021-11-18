42de37fbff0e756e39054de5b5783e9e6705a4e3
mrushidany <mrushidany@gmail.com>
2021-11-18 15:37:09 +0300

    Added the middleware auth sanctum in testing api route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/test_api', function (Request $request){
+Route::middleware('auth:sanctum')->post('/test_api', function (Request $request){
         return ['message' => $request->message ];
 });
