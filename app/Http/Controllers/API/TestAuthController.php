c591b00796710783a81e52e8a9906c3fe03a6b35
mrushidany <mrushidany@gmail.com>
2021-11-19 13:09:36 +0300

    TestLoginController works fine

--- app/Http/Controllers/API/TestAuthController.php
+++ app/Http/Controllers/API/TestAuthController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TestAuthController extends Controller
 {
-    //
+
+    public function login(Request $request)
+    {
+        //User checking if available
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            //Setting the logging response
+            $success['name'] = $user->name;
+            return response()->json([
+                'status' => 'success',
+                'data' => $success
+            ]);
+        }else {
+            return response()->json([
+                'status' => 'error',
+                'data' => 'Unauthorized Access'
+            ]);
+        }
+    }
 }
