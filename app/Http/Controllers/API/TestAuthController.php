28d8b083c67b18e922414ffcb97d69d9bcfc5121
mrushidany <mrushidany@gmail.com>
2021-11-24 11:15:57 +0300

    Added the cpanel yml file

--- app/Http/Controllers/API/TestAuthController.php
+++ app/Http/Controllers/API/TestAuthController.php
@@ -3,15 +3,72 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class TestAuthController extends Controller
 {
 
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255|unique:users,email',
+            'password' => 'required|string|min:4'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Credentials validation error.'
+            ]);
+        }
+
+
+        // if($registration){
+        //     return response()->json([
+        //         'status' => 'success',
+        //         'data' => 'Successful registered the user'
+        //     ], 200);
+        // }else{
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'data' => 'Registration process failed'
+        //     ]);
+        // }
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required','string', 'min:4']
+        ]);
+    }
+
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
     public function login(Request $request)
     {
+        $email = $request->post('email');
+        $password = $request->post('password');
         //User checking if available
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             $user = Auth::user();
@@ -24,36 +81,8 @@ class TestAuthController extends Controller
         }else {
             return response()->json([
                 'status' => 'error',
-                'data' => 'Unauthorized Access'
+                'data' => 'Unauthorized Access'.' Requests:  '.$email
             ]);
         }
-    }
-
-    public function register(Request $request)
-    {
-        $this->validator($request->all())->validate();
-        $user = $this->create($request->all());
-        $registration = $this->guard()->login($user);
-
-        if($registration){
-            return response()->json([
-                'status' => 'success',
-                'data' => 'Successful registered the user'
-            ]);
-        }else{
-            return response()->json([
-                'status' => 'error',
-                'data' => 'Registration process failed'
-            ]);
-        }
-    }
-
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required','string', 'min:4']
-        ]);
     }
 }
