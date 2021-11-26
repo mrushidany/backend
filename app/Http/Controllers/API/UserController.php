efd262601c0f86fe4fbdd08d4375ecab227b6ad1
mrushidany <mrushidany@gmail.com>
2021-11-26 09:53:57 +0300

    Authentication process implemented within the login method in the UserController

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $user_credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($user_credentials)) {
+            // Authentication passed
+
+            $authuser = auth()->user();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login successful'
+            ], 200);
+        }else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+    }
 }
