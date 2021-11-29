94a2963b895850acf2feb34dcbf4f2bc30e11a8a
mrushidany <mrushidany@gmail.com>
2021-11-29 18:25:32 +0300

    Troubleshoot the create function and including the dependency files

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    protected function create($data[])
+    protected function create(array $data)
     {
         return User::create([
             'name' => $data['name'],
