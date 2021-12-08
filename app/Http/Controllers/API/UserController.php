a54ab4b3c382b6177ee8666a4f8e9f82ad49a834
mrushidany <mrushidany@gmail.com>
2021-12-08 13:01:27 +0300

    Updated the registration process

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -16,6 +17,10 @@ class UserController extends Controller
     {
         $this->validator($request->all())->validate();
         $user = $this->create($request->all());
+        $user->attachRole($request->role);
+        event(new Registered($user));
+
+        Auth::login($user);
 
         if($user){
             return response()->json([
@@ -44,11 +49,12 @@ class UserController extends Controller
 
     protected function create(array $data)
     {
-        return User::create([
+        return $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password'])
         ]);
+
     }
 
     protected function guard()
